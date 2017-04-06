97d8c5e0e23ba158d129db413efcc581256fc60a
Conor Malagraph <cmalagr1@binghamton.edu>
2017-04-06 19:39:04 -0400

    Pushed new point cloud example

--- cpp_point_cloud_example/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/cpp_point_cloud_example/__/__/__/__/__/tango_gl/shaders.o.d
+++
@@ -1,8 +0,0 @@
-C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/cpp_point_cloud_example/__/__/__/__/__/tango_gl/shaders.o: \
-  C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/shaders.cc \
-  C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h \
-  C:/Users/Cmala/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
-
-C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h:
-
-C:/Users/Cmala/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
