2c2a6a8262ef65b47d020ebad2d18316e60e500f
Conor Malagraph <cmalagr1@binghamton.edu>
2017-04-10 15:19:34 -0400

    removing to push project from AS

--- Point Cloud Example/cpp_point_cloud_example/app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/cpp_point_cloud_example/__/__/__/__/__/tango_gl/src/shaders.o.d
+++
@@ -1,8 +0,0 @@
-C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/cpp_point_cloud_example/__/__/__/__/__/tango_gl/src/shaders.o: \
-  C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/src/shaders.cc \
-  C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h \
-  C:/Users/Cmala/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
-
-C:/Users/Cmala/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h:
-
-C:/Users/Cmala/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
