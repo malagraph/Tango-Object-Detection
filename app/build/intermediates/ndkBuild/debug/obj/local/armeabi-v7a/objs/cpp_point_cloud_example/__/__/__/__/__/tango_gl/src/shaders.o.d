e1a07d780d64ae26924107f15efa02eebbcb35c0
JordanLevin <jlevin38@binghamton.edu>
2017-04-15 23:44:11 -0400

    added some code to keep track of all points seen, unfinished
    Currently it keeps track of all points, and doesnt deltee old points. This causes it to break after about 2 seconds. Need to add code to replace old points with newer points

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/cpp_point_cloud_example/__/__/__/__/__/tango_gl/src/shaders.o.d
@@ -0,0 +1,8 @@
+C:/Users/Jordan/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/cpp_point_cloud_example/__/__/__/__/__/tango_gl/src/shaders.o: \
+  C:/Users/Jordan/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/src/shaders.cc \
+  C:/Users/Jordan/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h \
+  C:/Users/Jordan/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
+
+C:/Users/Jordan/Desktop/tango-examples-c-master/cpp_point_cloud_example/app/src/main/jni/../../../../../tango_gl/include\tango-gl/shaders.h:
+
+C:/Users/Jordan/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
